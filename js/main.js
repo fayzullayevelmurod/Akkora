86e9ed308962db48f074ce8e0a47f7147652098d
fayzullayevelmurod <elmurodfayzullayev77@gmail.com>
2024-03-29 17:29:59 +0500

    footer slider desktop the end

--- js/main.js
+++ js/main.js
@@ -3,4 +3,18 @@ var swiper = new Swiper(".popular_productSwiper", {
         nextEl: ".popular_slider-button-next",
         prevEl: ".popular_slider-button-prev",
     },
+    breakpoints: {
+        576: {
+            slidesPerView: 2,
+            spaceBetween: 20,
+        },
+        768: {
+            slidesPerView: 3,
+            spaceBetween: 20,
+        },
+        993: {
+            slidesPerView: 4,
+            spaceBetween: 22,
+        },
+    },
 });
