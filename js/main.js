0fffd77405242aac246918e1613439aeecb04b3b
fayzullayevelmurod <elmurodfayzullayev77@gmail.com>
2024-04-05 15:42:23 +0500

    tabs block 80% the end

--- js/main.js
+++ js/main.js
@@ -1,3 +1,20 @@
+const tabs = document.querySelectorAll('[data-tab-target]');
+const tabContents = document.querySelectorAll('[data-tab-content]');
+
+tabs.forEach(tab => {
+    tab.addEventListener('click', () => {
+        const target = document.querySelector(tab.dataset.tabTarget)
+        tabContents.forEach(tabContent => {
+            tabContent.classList.remove('active')
+        })
+        tabs.forEach(tab => {
+            tab.classList.remove('active')
+        })
+        tab.classList.add('active')
+        target.classList.add('active')
+    })
+});
+
 let swiper = new Swiper(".popular_productSwiper", {
     spaceBetween: 20,
     navigation: {
@@ -21,23 +38,4 @@ let swiper = new Swiper(".popular_productSwiper", {
 });
 
 
-try {
-    const tabs = document.querySelectorAll('[data-tab-target]');
-    const tabContents = document.querySelectorAll('[data-tab-content]');
 
-    tabs.forEach(tab => {
-    tab.addEventListener('click', () => {
-        const target = document.querySelector(tab.dataset.tabTarget)
-        tabContents.forEach(tabContent => {
-        tabContent.classList.remove('active')
-        })
-        tabs.forEach(tab => {
-        tab.classList.remove('active')
-        })
-        tab.classList.add('active')
-        target.classList.add('active')
-    })
-    })
-} catch(err) {
-    console.log(err);
-}
