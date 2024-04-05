596bc686cb49e14a6ed6519fc33520a41e6f70af
fayzullayevelmurod <elmurodfayzullayev77@gmail.com>
2024-04-05 15:12:52 +0500

    tabs in progress

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-var swiper = new Swiper(".popular_productSwiper", {
+let swiper = new Swiper(".popular_productSwiper", {
     spaceBetween: 20,
     navigation: {
         nextEl: ".popular_slider-button-next",
@@ -19,3 +19,25 @@ var swiper = new Swiper(".popular_productSwiper", {
         },
     },
 });
+
+
+try {
+    const tabs = document.querySelectorAll('[data-tab-target]');
+    const tabContents = document.querySelectorAll('[data-tab-content]');
+
+    tabs.forEach(tab => {
+    tab.addEventListener('click', () => {
+        const target = document.querySelector(tab.dataset.tabTarget)
+        tabContents.forEach(tabContent => {
+        tabContent.classList.remove('active')
+        })
+        tabs.forEach(tab => {
+        tab.classList.remove('active')
+        })
+        tab.classList.add('active')
+        target.classList.add('active')
+    })
+    })
+} catch(err) {
+    console.log(err);
+}
