45447dc37deac9413d35db39271b04c8a44bb0c9
fayzullayevelmurod <elmurodfayzullayev77@gmail.com>
2024-04-10 00:30:47 +0500

    select in progress

--- js/main.js
+++ js/main.js
@@ -32,6 +32,29 @@ dec.addEventListener("click", () => {
   console.log(err);
 }
 
+let index = 1;
+
+const on = (listener, query, fn) => {
+	document.querySelectorAll(query).forEach(item => {
+		item.addEventListener(listener, el => {
+			fn(el);
+		})
+	})
+}
+
+on('click', '.selectBtn', item => {
+	const next = item.target.nextElementSibling;
+	next.classList.toggle('toggle');
+	next.style.zIndex = index++;
+});
+on('click', '.option', item => {
+	item.target.parentElement.classList.remove('toggle');
+
+	const parent = item.target.closest('.select').children[0];
+	parent.setAttribute('data-type', item.target.getAttribute('data-type'));
+	parent.innerText = item.target.innerText;
+})
+
 
 const tabs = document.querySelectorAll('[data-tab-target]');
 const tabContents = document.querySelectorAll('[data-tab-content]');
